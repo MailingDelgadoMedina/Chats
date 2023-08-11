4b66326756b1f12a91405d675e28f1e9d74ba675
MailingDelgadoMedina <mailing.delgado.scitexas@gmail.com>
2023-08-10 20:23:03 -0400

    boiler added config file with websocketconfig.java

---
+++ src/main/java/com/mailingdelgadomedina/chats/WebsocketConfig.java
@@ -0,0 +1,25 @@
+package com.mailingdelgadomedina.chats;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
+
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer{
+ 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+       registry.setApplicationDestinationPrefixes("/app");
+         registry.enableSimpleBroker("/chatroom", "/user");
+         registry.setUserDestinationPrefix("/user");
+    }
+ @Override
+    registerStompEndpoints(StompEndpointRegistry registry) {
+     WebSocketMessageBrokerConfigurationSupport.super.registerStompEndpoints(registry);
+       registry.addEndpoint("/ws").setAllowedOriginsPatterns("*").withSockJS();
+       // registry.addEndpoint("/chat").withSockJS();
+   }
+}
